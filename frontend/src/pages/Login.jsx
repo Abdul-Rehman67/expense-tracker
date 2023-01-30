43bcd8ef6de51beb922fd3b0b6722a3025e7436e
Abdul <ar6373150@gmail.com>
2023-01-30 11:23:04 +0500

    login mobile UI and some function

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,122 +1,68 @@
-import React from "react";
+import {React,useState} from "react";
 import { Button, Checkbox, Form, Input } from "antd";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const onFinish = (values) => {
-    console.log("Success:", values);
+  const [formData, setFormData] = useState([]);
+  const handleSubmit = () => {
+    console.log(formData);
+    if (
+      Object.keys(formData).some((key) => formData[key] === "") ||
+      Object.keys(formData).length < 2
+    ) {
+      alert("all fields are required");
+    } else {
+      //api call using store
+    }
   };
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
   return (
     <>
-      <div class="bg-gray-200 min-h-screen flex flex-col">
-        <div class="container w-5/12 mx-auto flex-1 flex flex-col items-center justify-center px-2">
-          <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-            <h1 class="mb-8 text-3xl text-center">Login</h1>
+      <div className="bg-gray-200 min-h-screen flex flex-col">
+        <div className="container md:w-5/12 w-full mx-auto flex-1 flex flex-col items-center justify-center px-2">
+          <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+            <h1 className="mb-8 text-3xl text-center">Login</h1>
 
             <input
               type="text"
-              class="block border border-grey-light w-full p-3 rounded mb-4"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
               name="email"
               placeholder="Email"
+              onChange={handleChange}
             />
 
             <input
               type="password"
-              class="block border border-grey-light w-full p-3 rounded mb-4"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
               name="password"
               placeholder="Password"
+              onChange={handleChange}
+
             />
 
             <button
               type="submit"
-              class="w-full text-center py-3 rounded bg-blue-600 text-white hover:bg-blue-500 focus:outline-none my-1"
+              className="w-full text-center py-3 rounded bg-blue-600 text-white hover:bg-blue-500 focus:outline-none my-1"
+              onClick={handleSubmit}
             >
               Login
             </button>
-          <div class="text-grey-dark mt-6">
-            Don't have an account?
-            <a class="text-blue-500 no-underline border-b border-blue-500 text-blue cursor-pointer">
-              Signup
-            </a>
+            <div className="text-grey-dark mt-6">
+              Don't have an account?
+              <Link to={'/signup'}>
+                <span className="text-blue-500 no-underline border-b border-blue-500 text-blue cursor-pointer">
+                  Sign up
+                </span>
+              </Link>
+            </div>
           </div>
-          </div>
-
         </div>
       </div>
-      {/* <div class="bg-gray-200 min-h-screen flex flex-col">
-        <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-          <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-            <Form
-              name="basic"
-              labelCol={{
-                span: 8,
-              }}
-              wrapperCol={{
-                span: 16,
-              }}
-              style={{
-                maxWidth: 600,
-              }}
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              autoComplete="off"
-            >
-              <Form.Item
-                label="Username"
-                name="username"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your username!",
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
-
-              <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your password!",
-                  },
-                ]}
-              >
-                <Input.Password />
-              </Form.Item>
-
-              <Form.Item
-                name="remember"
-                valuePropName="checked"
-                wrapperCol={{
-                  offset: 8,
-                  span: 16,
-                }}
-              >
-                <Checkbox>Remember me</Checkbox>
-              </Form.Item>
-
-              <Form.Item
-                wrapperCol={{
-                  offset: 8,
-                  span: 16,
-                }}
-              >
-                <Button type="primary" htmlType="submit">
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
